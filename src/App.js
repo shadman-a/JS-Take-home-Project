1e391ac0f78b127a2517447b0c7a4772ed484fc7
shadman-a <shadman@outlook.com>
2020-11-14 13:54:43 -0500

    2nd

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from "react";
-
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button'
 
 export default class App extends React.Component {
   
+  state = {
+    render: false
+	};
+
 
   componentDidMount() {
-    setTimeout(function() { //Start the timer
-        this.setState({render: true}) //After 1 second, set render to true
-    }.bind(this), 1000)
+    setTimeout(function() { 
+        this.setState({render: true}) 
+    }.bind(this), 5000)
+  }
+
+  handleClose = () =>{
+    this.setState({
+      render: false
+    })
   }
   
+  modal = () => {
+      return (
+        <Modal show={this.state.render} onHide={()=>this.handleClose()}>
+        <Modal.Header closeButton>
+          <Modal.Title>We've got a deal for you</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Click below for your very special offer, just for you</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={()=>this.handleClose()}>
+            Claim Now
+          </Button>
+        </Modal.Footer>
+      </Modal>
+     );
+    
+  };
   
   render(){
+
     return (
+      <>
       <div className="App">
-        <h1>Hello World</h1>
+        <h1>Public Goods</h1>
       </div>
+       {this.modal()}
+       </>
     );
     }
 
   
 }
 
-export default App;
