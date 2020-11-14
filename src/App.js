7308bcfb5f31ae3d4b849e319a76b036a91f1ff0
shadman-a <shadman@outlook.com>
2020-11-14 13:34:30 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  
+
+  componentDidMount() {
+    setTimeout(function() { //Start the timer
+        this.setState({render: true}) //After 1 second, set render to true
+    }.bind(this), 1000)
+  }
+  
+  
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+    }
+
+  
 }
 
 export default App;
